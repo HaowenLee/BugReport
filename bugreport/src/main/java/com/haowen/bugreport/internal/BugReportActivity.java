b7297ed37903526044434168809c0f0625fe9afc
HaowenLee <haowenhello@gmail.com>
2019-05-30 11:18:43 +0800

    modify ui

--- bugreport/src/main/java/com/haowen/bugreport/internal/BugReportActivity.java
+++ bugreport/src/main/java/com/haowen/bugreport/internal/BugReportActivity.java
@@ -10,6 +10,7 @@ import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.TypefaceSpan;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.haowen.bugreport.Config;
@@ -18,13 +19,18 @@ import com.haowen.bugreport.R;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+/**
+ * 崩溃信息显示
+ */
 public class BugReportActivity extends Activity {
 
     public static final String STACKTRACE = "bugreport.stacktrace";
     private TextView tvThread;
     private TextView tvException;
     private TextView tvExceptionDesc;
-    private TextView reportTextView;
+    private TextView tvReportDetail;
+    private Button btnSend;
+    private Button btnCancel;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -32,7 +38,56 @@ public class BugReportActivity extends Activity {
         setContentView(R.layout.com_haowen_activity_bug_report);
 
         findViewByIds();
+        initListener();
+        setCrashDetail2UI();
+    }
 
+    /**
+     * findViewById
+     */
+    private void findViewByIds() {
+        tvThread = findViewById(R.id.tvThread);
+        tvException = findViewById(R.id.tvException);
+        tvExceptionDesc = findViewById(R.id.tvExceptionDesc);
+        tvReportDetail = findViewById(R.id.tvReportDetail);
+        btnSend = findViewById(R.id.btnSend);
+        btnCancel = findViewById(R.id.btnCancel);
+    }
+
+    /**
+     * 监听按钮点击事件
+     */
+    private void initListener() {
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{Config.DEVELOPER_EMAIL});
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, tvReportDetail.getText().toString());
+                    sendIntent.putExtra(Intent.EXTRA_SUBJECT, Utils.getAppInfo(BugReportActivity.this));
+                    sendIntent.setType("message/rfc822");
+                    startActivity(sendIntent);
+                } catch (ActivityNotFoundException e) {
+                    // Empty
+                } finally {
+                    finish();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    /**
+     * 设置崩溃信息到UI
+     */
+    private void setCrashDetail2UI() {
         Throwable exception = (Throwable) getIntent().getSerializableExtra(STACKTRACE);
 
         Throwable causeThrowable = CrashUtils.getCauseThrowable(exception);
@@ -54,41 +109,15 @@ public class BugReportActivity extends Activity {
         String content = stackTrace.toString();
         builder.append(content);
 
-        reportTextView.setText(builder);
-
-        findViewById(R.id.send_report).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Intent sendIntent = new Intent(Intent.ACTION_SEND);
-                    sendIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{Config.DEVELOPER_EMAIL});
-                    sendIntent.putExtra(Intent.EXTRA_TEXT, reportTextView.getText().toString());
-                    sendIntent.putExtra(Intent.EXTRA_SUBJECT, Utils.getAppInfo(BugReportActivity.this));
-                    sendIntent.setType("message/rfc822");
-                    startActivity(sendIntent);
-                } catch (ActivityNotFoundException e) {
-                    // Empty
-                } finally {
-                    finish();
-                }
-            }
-        });
-
-        findViewById(R.id.cancel_report).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        tvReportDetail.setText(builder);
     }
 
-    private void findViewByIds() {
-        tvThread = findViewById(R.id.tvThread);
-        tvException = findViewById(R.id.tvException);
-        tvExceptionDesc = findViewById(R.id.tvExceptionDesc);
-        reportTextView = findViewById(R.id.report_text);
-    }
-
+    /**
+     * 高亮内容
+     *
+     * @param content 内容
+     * @return SpannableString对象
+     */
     private SpannableString highlightSpan(String content) {
         SpannableString highlightSpan = new SpannableString(content);
         ForegroundColorSpan textColorSpan = new ForegroundColorSpan(0xff42a5f5);
